07434edab16a9f6ff27a178ac76fc46e4fb655fa
Ant <ryu4979@gmail.com>
2019-07-04 23:13:27 +0900

    fix: リポジトリ名変更

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@ var config = {
   // and branch to get files from.
   github: {
     username: 'Ant2357',
-    repo: 'cms.js-starter',
+    repo: 'ant-blog',
     branch: 'gh-pages',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
