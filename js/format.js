7bc02e0ab926a3072cab945e713932709727d15b
Ant <ryu4979@gmail.com>
2020-04-21 01:59:25 +0900

    fix: 日付のバグ修正

--- js/format.js
+++ js/format.js
@@ -1,2 +1,10 @@
 
-const jpDateConversion = str => str.split('/').reverse().join('/');
+const jpDateConversion = dateStr => {
+  const jpDateStr = dateStr.split('/').reverse().join('/');
+  const jpDateStrArr = jpDateStr.split('/');
+
+  const now = new Date(jpDateStrArr[0], jpDateStrArr[1] - 1, jpDateStrArr[2]);
+  const prevDate = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1);
+
+  return `${prevDate.getFullYear()}/${prevDate.getMonth() + 1}/${prevDate.getDate()}`;
+}
